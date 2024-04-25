07d77153d97fb99a3be6637bf6f4d23820c573a1
JanYork <747945307@qq.com>
2024-04-25 17:30:18 +0800

    feat(enum): logger 日志级别枚举

---
+++ src/enum/logger.enum.ts
@@ -0,0 +1,14 @@
+/**
+ * Enum for log levels
+ *
+ * @enum {number} LogLevel
+ */
+enum LogLevel {
+    DEBUG = 0,
+    INFO = 1,
+    WARN = 2,
+    ERROR = 3,
+    OFF = 4
+}
+
+export default LogLevel;
